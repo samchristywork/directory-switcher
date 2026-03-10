6b6eab07476c8a475c1fa3706018fa0591fca396
samchristywork <106397573+samchristywork@users.noreply.github.com>
2026-03-09 20:58:15 -0500

    feat: Update fish install script to use a function

--- scripts/add-alias-fish.fish
+++ scripts/add-alias-fish.fish
@@ -2,12 +2,23 @@
 
 set binary (realpath (dirname (status filename))/../target/release/directory-switcher)
 set config_file ~/.config/fish/config.fish
-set alias_line "alias ds '$binary && cd (cat /tmp/directory-switcher-\$fish_pid 2>/dev/null) ; rm -f /tmp/directory-switcher-\$fish_pid'"
 
-if grep -qF "alias ds" $config_file 2>/dev/null
-    echo "ds alias already present in $config_file"
+set func_text "
+function ds
+    set out /tmp/directory-switcher-\$fish_pid
+    $binary
+    if test -f \$out
+        set target (cat \$out)
+        rm -f \$out
+        if test -n \"\$target\"
+            cd \$target
+        end
+    end
+end"
+
+if grep -qF "function ds" $config_file 2>/dev/null
+    echo "ds function already present in $config_file"
 else
-    echo "" >> $config_file
-    echo $alias_line >> $config_file
-    echo "Added ds alias to $config_file"
+    echo $func_text >> $config_file
+    echo "Added ds function to $config_file"
 end
